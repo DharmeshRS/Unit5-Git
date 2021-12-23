7ed4887062ded29b0e74fd35fa7877622ca90786
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2021-12-23 22:02:56 +0530

    Update App.js

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,26 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// const arr1=["Android","Blackberry","iPhone","window phone"]
+function Mobileheading(){
+  return <h1>Mobile Operating System</h1>
+}
 
-function App() {
-  return (
-    <div className="App">
-    <div>
-      <h1>Mobile Operating System</h1>
-      <ul>
-        <li>Android</li>
-        <li>Blackberry</li>
-        <li>iPhone</li>
-        <li>Windows Phone</li>
-      </ul>
-    </div>
-    <div>
-      <h1>Mobile Manufacturer</h1>
-      <ul>
+function Firstarray(){
+  return (<ul>
+        <li type="square">Samsung</li>
+        <li type="square">HTC</li>
+        <li>Micromax</li>
+        <li type="circle">Apple</li>
+      </ul>)
+}
+function Secheading(){
+  return <h1>Mobile Manufacturer</h1>
+}
+
+function Secarray(){
+  return <ul>
         <li type="square">Samsung</li>
         <li type="square">HTC</li>
         <li>Micromax</li>
         <li type="circle">Apple</li>
       </ul>
+}
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <Mobileheading />
+      <Firstarray />
+    </div>
+    <div>
+      <Secheading />
+      <Secarray />
     </div>
     
     </div>
