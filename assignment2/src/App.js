e67d0fc3d27d06d7c40ee47334bc2c39ec96a1a9
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2021-12-23 22:11:57 +0530

    Update App.js

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,16 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
-// const arr1=["Android","Blackberry","iPhone","window phone"]
+const arr1=["Android","Blackberry","iPhone","window phone"]
 function Mobileheading(){
   return <h1>Mobile Operating System</h1>
 }
 
 function Firstarray(){
   return (<ul>
-        <li type="square">Samsung</li>
-        <li type="square">HTC</li>
-        <li>Micromax</li>
-        <li type="circle">Apple</li>
+        {arr1.map((e)=>(
+          <li type="square">{e}</li>
+        ))}
+       
       </ul>)
 }
 function Secheading(){
