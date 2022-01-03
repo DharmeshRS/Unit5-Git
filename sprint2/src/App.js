5452bb7b45976a7a9096440b819c852d18d97cc4
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2022-01-03 14:52:50 +0530

    first commit

--- sprint2/src/App.js
+++ sprint2/src/App.js
@@ -4,6 +4,40 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+            <div className='Div1'>
+              <div>
+                  <h1>Add recipe</h1>
+              </div>
+              <div>
+                  <form>
+                  <div>
+                      <label>Title</label>
+                      <input type="text" placeholder="Enter Title Of recipe" />
+                  </div>
+                  <div>
+                      <label>Ingredients</label>
+                      <input type="text" placeholder="Ingredients" />
+                  </div>
+                  <div>
+                      <label>Time to cook</label>
+                      <input type="text" placeholder="Time To cook" />
+                  </div>
+                  <div>
+                  <label>Image</label>
+                      <input type="file" />
+                  </div>
+                  
+                      
+                  </form>
+              </div>
+            </div>
+
+
+
+
+            <div className='Div2'>
+              <h1>Description</h1>
+            </div>
             
     </div>
   );
