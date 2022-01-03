68d827b6d7371251829e6b2d82b13a8516515839
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2022-01-03 15:13:40 +0530

    Update App.js

--- sprint2/src/App.js
+++ sprint2/src/App.js
@@ -1,9 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [allData,setAllData]=useState({
+    title:"",
+    ingredient:"",
+    time:"",
+    // image:""
+    instruction:""
+  })
+
+  const changeData=(event)=>{
+      const name=event.target.name;
+      const value=event.target.value;
+      setAllData((prev)=>{
+        return{
+          ...prev,
+          [name]:value,
+        }
+      })
+  }
+
+  const onsubmits=(event)=>{
+    event.preventDefault();
+    alert("Data Saved")
+  }
   return (
-    <div className="App">
+    <div className="App" onSubmit={onsubmits}>
             <div className='Div1'>
               <div>
                   <h1>Add recipe</h1>
@@ -12,21 +36,27 @@ function App() {
                   <form>
                   <div>
                       <label>Title</label>
-                      <input type="text" placeholder="Enter Title Of recipe" />
+                      <input type="text" placeholder="Enter Title Of recipe" name='title' onChange={changeData}/>
                   </div>
                   <div>
                       <label>Ingredients</label>
-                      <input type="text" placeholder="Ingredients" />
+                      <input type="text" placeholder="Ingredients" name="ingredient" onChange={changeData} />
                   </div>
                   <div>
                       <label>Time to cook</label>
-                      <input type="text" placeholder="Time To cook" />
+                      <input type="text" placeholder="Time To cook" name='time' onChange={changeData} />
                   </div>
-                  <div>
+                  {/* <div>
                   <label>Image</label>
                       <input type="file" />
+                  </div> */}
+                  <div>
+                  <label>Instructions</label>
+                      <input type="text" placeholder='Instructions' name='instruction' onChange={changeData}/>
                   </div>
-                  
+                  <div>
+                    <button type="submit">submit</button>
+                  </div>
                       
                   </form>
               </div>
