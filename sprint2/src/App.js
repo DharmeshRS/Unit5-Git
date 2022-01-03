23caadace17e5dbba9ea70287b2f1f20dc27e5d6
Dharmesh Rajendra Sonar <55545639+DharmeshRS@users.noreply.github.com>
2022-01-03 17:40:14 +0530

    supdate

--- sprint2/src/App.js
+++ sprint2/src/App.js
@@ -9,16 +9,7 @@ function App() {
      const {name,value}=event.target
      setAllData({...allData,[name]:value})
   }
-  // useEffect(() => {
-  //  fetch('http://localhost:8000/recipe')
-  //  .then(res=>{
-  //    return res.json()
-  //  })
-  //  .then(data=>{
-  // console.log(data)
-  // setAllData(data)
-  //  })
-  // }, [])
+  
   const onsubmits=(event)=>{
     event.preventDefault();
     console.log(allData)
@@ -29,7 +20,7 @@ function App() {
         "content-type":"application/json",
       },
     }).then(()=>{
-      setAllData({});
+      setAllData("");
       alert("recipe Uploaded...")
     })
 
@@ -41,71 +32,81 @@ function App() {
     .then((res)=>res.json())
     .then((data)=>setList(data))
     
-        
-    
+      const [details,setDetails]=useState({})
+      
+      const showData=(ddata)=>{
+        setDetails(ddata)
+        console.log(ddata)
+      }
 
 
   return (
     <div className="App" onSubmit={onsubmits}>
-            <div className='Div1'>
-              <div>
-                  <h1>Add recipe</h1>
-              </div>
-              <div>
-                  <form>
+    <div>
+        <div className='Div1'>
                   <div>
-                      <label>Title</label>
-                      <input type="text" placeholder="Enter Title Of recipe" name='title' onChange={changeData}/>
+                      <h1>Add recipe</h1>
                   </div>
                   <div>
-                      <label>Ingredients</label>
-                      <input type="text" placeholder="Ingredients" name="ingredient" onChange={changeData} />
+                      <form>
+                      <div>
+                          <label>Title</label>
+                          <input type="text" placeholder="Enter Title Of recipe" name='title' onChange={changeData}/>
+                      </div>
+                      <div>
+                          <label>Ingredients</label>
+                          <input type="text" placeholder="Ingredients" name="ingredient" onChange={changeData} />
+                      </div>
+                      <div>
+                          <label>Time to cook</label>
+                          <input type="text" placeholder="Time To cook" name='time' onChange={changeData} />
+                      </div>
+                      <div>
+                      <label>Image</label>
+                          <input type="text" placeholder="enter url" name='img' onChange={changeData} />
+                      </div>
+                      <div>
+                      <label>Instructions</label>
+                          <textarea rows={6} type="text" placeholder='Instructions' name='instruction' onChange={changeData}/>
+                      </div>
+                      <div>
+                        <button type="submit">submit</button>
+                      </div>
+                          
+                      </form>
                   </div>
-                  <div>
-                      <label>Time to cook</label>
-                      <input type="text" placeholder="Time To cook" name='time' onChange={changeData} />
-                  </div>
-                  <div>
-                  <label>Image</label>
-                      <input type="text" placeholder="enter url" name='img' onChange={changeData} />
-                  </div>
-                  <div>
-                  <label>Instructions</label>
-                      <input type="text" placeholder='Instructions' name='instruction' onChange={changeData}/>
-                  </div>
-                  <div>
-                    <button type="submit">submit</button>
-                  </div>
-                      
-                  </form>
-              </div>
+                </div>
+                <div className='Div2'>
+                  <h1>Description</h1>
+                
+                <div>
+                {list.map((re)=>{
+                    return (
+                      <div style={{border:"1px solid black"}} className='innerdiv'  onClick={showData} details={details}>
+                        <div className='innerSpan1'>
+                            {re.title}
+                        </div>
+                     
+                        <div className='innerSpan2'>
+                            {re.time}
+                    </div>
+                    <div className='innerSpan3'>
+                            <img src={re.img} style={{width:"150px"}}/>
+                    </div>
+                      </div>)
+                })}
             </div>
-
-
-
-
-            <div className='Div2'>
-              <h1>Description</h1>
-            
-            <div>
-            {list.map((re)=>{
-                return (
-                  <div style={{border:"1px solid black"}}>
-                    <span style={{border:"1px solid black"}}>
-                        {re.title}
-                    </span>
-                   &nbsp;&nbsp;
-                    <span>
-                        {re.time}
-                </span>
-                   </div>)
-            })}
         </div>
-    </div>
+    </div>            
     <div>
       All Information
     </div>
     </div>
+
+
+
+           
+   
   );
 }
 
